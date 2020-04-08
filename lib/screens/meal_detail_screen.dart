cd77d318e2c1846397431129b96e080c1d1f688c
JyotinderSingh <jyotindrsingh@gmail.com>
2020-04-08 18:47:54 +0530

    more UI enhancements

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -28,7 +28,7 @@ class MealDetailScreen extends StatelessWidget {
   Widget buildContainer(Widget child, double height) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10),
-      margin: EdgeInsets.only(top: 5, left: 15, right: 15, bottom: 15),
+      margin: EdgeInsets.only(top: 5, left: 5, right: 5, bottom: 15),
       height: height,
       decoration: BoxDecoration(
           // border:
@@ -47,80 +47,78 @@ class MealDetailScreen extends StatelessWidget {
     final mealId = ModalRoute.of(context).settings.arguments as String;
     final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-        appBar: AppBar(
-          title: Text('${selectedMeal.title}'),
-        ),
-        body: Scrollbar(
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  height: 250,
-                  width: double.infinity,
-                  child: Image.network(
-                    selectedMeal.imageUrl,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                buildSctionTitle('Ingredients'),
-                buildContainer(
-                    ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (ctx, index) => Container(
-                        width: 150,
-                        child: Card(
-                          margin:
-                              EdgeInsets.symmetric(horizontal: 7, vertical: 2),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
+      appBar: AppBar(
+        title: Text('${selectedMeal.title}'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 250,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            buildSctionTitle('Ingredients'),
+            buildContainer(
+                ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (ctx, index) => Container(
+                    width: 150,
+                    child: Card(
+                      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      elevation: 3,
+                      color: Theme.of(context).accentColor,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          top: 25,
+                          bottom: 20,
+                          left: 15,
+                          right: 15,
+                        ),
+                        child: Text(
+                          selectedMeal.ingredients[index],
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.bold,
                           ),
-                          elevation: 3,
-                          color: Theme.of(context).accentColor,
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                              top: 25,
-                              bottom: 20,
-                              left: 15,
-                              right: 15,
-                            ),
-                            child: Text(
-                              selectedMeal.ingredients[index],
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontFamily: 'Raleway',
-                                fontWeight: FontWeight.bold,
-                              ),
-                              softWrap: true,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
+                          softWrap: true,
+                          overflow: TextOverflow.fade,
                         ),
                       ),
-                      itemCount: selectedMeal.ingredients.length,
                     ),
-                    100),
-                buildSctionTitle('Steps'),
-                buildContainer(
-                    ListView.builder(
-                      itemBuilder: (ctx, index) => Column(children: [
-                        ListTile(
-                          leading: CircleAvatar(
-                            child: Text('# ${(index + 1)}'),
-                          ),
-                          title: Text(
-                            selectedMeal.steps[index],
-                          ),
-                        ),
-                        Divider(
-                          thickness: 1,
-                        ),
-                      ]),
-                      itemCount: selectedMeal.steps.length,
+                  ),
+                  itemCount: selectedMeal.ingredients.length,
+                ),
+                100),
+            buildSctionTitle('Steps'),
+            buildContainer(
+                ListView.builder(
+                  itemBuilder: (ctx, index) => Column(children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('# ${(index + 1)}'),
+                      ),
+                      title: Text(
+                        selectedMeal.steps[index],
+                      ),
                     ),
-                    400)
-              ],
-            ),
-          ),
-        ));
+                    Divider(
+                      thickness: 1,
+                    ),
+                  ]),
+                  itemCount: selectedMeal.steps.length,
+                ),
+                400)
+          ],
+        ),
+      ),
+    );
   }
 }
